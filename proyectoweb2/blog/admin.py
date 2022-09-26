0b22623988afd2a876c4206b74f9382b5a6816f4
argenis <argenis.salazar.sihuay@gmail.com>
2022-09-25 21:30:03 -0500

    se agrega el segundo cambio Master2

--- proyectoweb2/blog/admin.py
+++ proyectoweb2/blog/admin.py
@@ -6,9 +6,14 @@ from .models import Categoria,Post
 
 class CategoriaAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
+    list_display=("id","nombre","created","updated")
+    search_fields=("id","nombre")
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields=('created','updated')
+    list_display=("id","titulo","Contenido","autor","created","updated")
+    search_fields=("id","titulo")
+    list_filter=("autor",)
     
 admin.site.register(Categoria,CategoriaAdmin)
 admin.site.register(Post, PostAdmin)
